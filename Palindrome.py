86a03fd32a17b3a26a681c38ec2aaffb6394a8a8
saicharan-o <saicharanr227@gmail.com>
2026-04-29 23:26:12 +0530

    Update Palindrome file

--- Palindrome.py
+++ Palindrome.py
@@ -1,5 +1,6 @@
 def isPalindrome(x):
     return str(x)==str(x)[::-1] 
+    
 print(isPalindrome(123))
 print(isPalindrome(424))
 print(isPalindrome(-121))
@@ -8,14 +9,14 @@ print(isPalindrome(232))
 print(isPalindrome(999))
 print(isPalindrome(-333))
 print(isPalindrome(9929))
-print(isPalindrome(2020202))
+print(isPalindrome(20202))
 
 print(isPalindrome(-3953))
 print(isPalindrome(-2))
 print(isPalindrome(9))
-print(isPalindrome(889988))
+print(isPalindrome(88998))
 
 print(isPalindrome(100))
 print(isPalindrome(3459))
-print(isPalindrome(149941))
-print(isPalindrome(0))
+print(isPalindrome(19941))
+print(isPalindrome(04))
